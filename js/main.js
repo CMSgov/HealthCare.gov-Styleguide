e0ce9524dc0d52db6690586c276581116398c6e1
s9vm <james.smith@aquilent.com>
2015-03-18 17:47:31 -0400

    Assets landing page, click to clipboard, and some code cleanup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 (function(){
-	
+
 })();
+
+$(document).ready(function() {
+	var clip = new ZeroClipboard($(".copy-button"));
+
+  	clip.on("ready", function() {
+    	//console.log("Flash movie loaded and ready.");
+
+		this.on("aftercopy", function(event) {
+      		//console.log("Copied text to clipboard: " + event.data["text/plain"]);
+    	});
+  	});
+
+	clip.on("error", function(event) {
+    	$(".demo-area").hide();
+    	console.og('error[name="' + event.name + '"]: ' + event.message);
+    	ZeroClipboard.destroy();
+	});
+});
