5eed949261629674cd0f2e9099e1ffcef7019278
s9vm <james.smith@aquilent.com>
2015-03-23 15:42:18 -0400

    Fixed typo in zero clipboard js code. console.log was console.og. Cleaned up whitespace.

--- js/main.js
+++ js/main.js
@@ -9,14 +9,14 @@ $(document).ready(function() {
   	clip.on("ready", function() {
     	//console.log("Flash movie loaded and ready.");
 
-		this.on("aftercopy", function(event) {
-      		//console.log("Copied text to clipboard: " + event.data["text/plain"]);
+		  this.on("aftercopy", function(event) {
+      	//console.log("Copied text to clipboard: " + event.data["text/plain"]);
     	});
-  	});
+  });
 
 	clip.on("error", function(event) {
   	$(".demo-area").hide();
-  	console.og('error[name="' + event.name + '"]: ' + event.message);
+  	console.log('error[name="' + event.name + '"]: ' + event.message);
   	ZeroClipboard.destroy();
   });
 
